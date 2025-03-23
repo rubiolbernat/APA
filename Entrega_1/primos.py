b0c2bc754eb30f66b2b2164c15fbcb787e09ac17
BR <rubiolbernat@gmail.com>
2025-03-23 18:48:08 +0100

    Add functions for prime number calculations and update tests

--- Entrega_1/primos.py
+++ Entrega_1/primos.py
@@ -1,11 +1,29 @@
 """
 Bernat Rubiol
 
->>>esprimer(4)
+>>> esprimer(4)
 False
 
->>>esprimer(-2)
+>>> esprimer(-2)
 True
+
+>>> primers(50)
+(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47)
+
+>>> descompon(36 * 175 * 143)
+(2, 2, 3, 3, 5, 5, 7, 11, 13)
+
+>>> mcm(90, 14)
+630
+
+>>> mcd(924, 780)
+12
+
+>>> mcmN(42, 60, 70, 63)
+1260
+
+>>> mcdN(840, 630, 1050, 1470)
+210
 """
 
 def esprimer(num):
@@ -17,3 +35,56 @@ def esprimer(num):
             return False
         
     return True
+
+def primers(num):
+    """Retorna una tupla amb tots els nombres primers menors que num."""
+    primers_trobats = []
+    for n in range(2, num):
+        if esprimer(n):
+            primers_trobats.append(n)
+    return tuple(primers_trobats)
+
+def descompon(num):
+    """Retorna una tupla amb la descomposició en factors primers de num."""
+    factors = []
+    div = 2
+    while num > 1:
+        while num % div == 0:
+            factors.append(div)
+            num //= div
+        div += 1
+    return tuple(factors)
+
+def mcm(num1, num2):
+    """Retorna el mínim comú múltiple de dos nombres."""
+    factors1 = descompon(num1)
+    factors2 = descompon(num2)
+    factors_comuns = set(factors1) | set(factors2)
+    resultat = 1
+    for factor in factors_comuns:
+        resultat *= factor ** max(factors1.count(factor), factors2.count(factor))
+    return resultat
+
+def mcd(num1, num2):
+    """Retorna el màxim comú divisor de dos nombres."""
+    factors1 = descompon(num1)
+    factors2 = descompon(num2)
+    factors_comuns = set(factors1) & set(factors2)
+    resultat = 1
+    for factor in factors_comuns:
+        resultat *= factor ** min(factors1.count(factor), factors2.count(factor))
+    return resultat
+
+def mcmN(*numeros):
+    """Retorna el mínim comú múltiple d'un nombre arbitrari d'arguments."""
+    resultat = numeros[0]
+    for num in numeros[1:]:
+        resultat = mcm(resultat, num)
+    return resultat
+
+def mcdN(*numeros):
+    """Retorna el màxim comú divisor d'un nombre arbitrari d'arguments."""
+    resultat = numeros[0]
+    for num in numeros[1:]:
+        resultat = mcd(resultat, num)
+    return resultat
--- Entrega_1/test_primos.py
+++ Entrega_1/test_primos.py
@@ -10,6 +10,21 @@ print("es primer" if result else "No es primer")
 for i in range(1, 100):
     print(f"{i}: ", pr.esprimer(i))
     
-pr.__doc__
+# Obtenir tots els nombres primers fins a un cert valor
+print("Primers fins a 50:", pr.primers(50))
+
+# Descomposició en factors primers
+num = 36 * 175 * 143
+print(f"Descomposició en factors primers de {num}: {pr.descompon(num)}")
+
+# Càlcul del mínim comú múltiple i màxim comú divisor per dos nombres
+print("MCM de 90 i 14:", pr.mcm(90, 14))
+print("MCD de 924 i 780:", pr.mcd(924, 780))
+
+# Càlcul del mínim comú múltiple i màxim comú divisor per múltiples nombres
+print("MCM de 42, 60, 70, 63:", pr.mcmN(42, 60, 70, 63))
+print("MCD de 840, 630, 1050, 1470:", pr.mcdN(840, 630, 1050, 1470))
+
+# Consultar la documentació del mòdul
 print(pr.__doc__)
 help(pr)
